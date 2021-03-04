28b865a90d9640453935b371bb62ed1ac265eb07
David Bouscarle <d.bouscarle@gmail.com>
2021-03-04 07:11:07 +0100

    Waiting for a parse soluce

--- contact.php
+++ contact.php
@@ -1,3 +1,23 @@
 <?php
 
     print_r($_POST['user']);
+
+    function getUser(): array
+    {
+
+        $user           = $_POST['user'];
+        $userIndex      = count($user);
+        $userFormatted  = [];
+
+        for ($i = 0; $i < $userIndex; $i++) { 
+            $userFormatted [] = [
+                $user[$i]
+                // $user[$i + 1],
+                // $user[$i + 2]
+            ];
+        }
+
+        return $userFormatted;
+    }
+
+    print_r(getUser());
