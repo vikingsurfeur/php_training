36cfc21c134d0775e5aea867a1f8753267cef3c9
David Bouscarle <d.bouscarle@gmail.com>
2021-03-04 11:32:57 +0100

    server and env request

--- php_serv_env_session/php_serv.php
+++ php_serv_env_session/php_serv.php
@@ -1,5 +1,24 @@
 <?php
 
-foreach ($_SERVER as $key => $value) {
-    echo "[$key] = $value <br/>";
+// phpinfo(); This fonction get many information of server
+
+var_dump($_ENV); // $_ENV get some many information about environment of execution / not hydrated here
+// getenv() get some many information as variable if not hydrated
+
+echo '<h2>$_ENV ARRAY</h2>';
+
+foreach (getenv() as $envKey => $envValue) {
+    echo "[$envKey] = $envValue <br>";
 }
+
+echo '<h2>$_SERVER ARRAY</h2>';
+
+foreach ($_SERVER as $key => $value) {
+    echo "[$key] = $value <br>";
+}
+
+// ACCESS TO THE ENV VARIABLE
+
+$_ENV['DATABASE_NAME'];
+// Or
+getenv('DATABAS_NAME');
