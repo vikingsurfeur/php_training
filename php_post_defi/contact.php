3224c30e3b622cf889cc0656d70b9018b69cedcf
David Bouscarle <d.bouscarle@gmail.com>
2021-03-04 22:03:24 +0100

    how to push the $_POST array in file?...

--- php_post_defi/contact.php
+++ php_post_defi/contact.php
@@ -1,1 +1,46 @@
+<?php
+
+function readContactFile()
+{
+    $file = 'contact.txt';
+    if (!is_file($file)) {
+        return null;
+    }
+    return file_get_contents($file);
+}
+
+function writeInContactFile($message)
+{
+    $file = 'contact.txt';
+    if (!is_file($file)) {
+        $current = '';
+    } else {
+        $current = file_get_contents($file);
+    }
+    $current .= $message;
+    $current .= PHP_EOL;
+    file_put_contents($file, $current);
+}
+
+function sendUserMessage() {
+    if ((!empty($_POST['name']) && !empty(trim($_POST['name']))) && 
+            (!empty($_POST['email']) && !empty(trim($_POST['email']))) && 
+                (!empty($_POST['message']) && !empty(trim($_POST['message'])))) {
+        echo require_once 'message.html';
+    } else {
+        echo 'Vous n\'avez pas renseigné tous les champs, veuillez resaisir les informations';
+    }
+}
+
+// CALL THE FUNCTIONS
+
+readContactFile();
+writeInContactFile($_POST['message']);
+sendUserMessage();
+
+// LOOP FOR A NEW ARRAY
+
+foreach ($_POST as $key => $value) {
+    echo "$key => $value <br>";
+}
 
