06053194678df48c5ef7d64a091d82f015490904
David Bouscarle <d.bouscarle@gmail.com>
2021-03-08 13:19:37 +0100

    Date ok - Begin files handling

--- date/introduction.php
+++ date/introduction.php
@@ -29,11 +29,19 @@ echo $microTimeAfter - $microTimeBefore; // O with XAMPP
 
 // MKTIME function
 
-$heure      = 12;
-$minutes    = 58;
-$secondes   = 35;
-$mois       = 12;
-$jour       = 31;
-$annee      = 2020;
+$heure      = 0;
+$minutes    = 0;
+$secondes   = 0;
+$mois       = 1;
+$jour       = 1;
+$annee      = 0;
 echo mktime($heure, $minutes, $secondes, $mois, $jour, $annee).'<br>'; // Give the timestamp of this current date // in this sort
 
+// DATE function + MKTIME function
+
+echo 'Premier de l\'an 2000 : '.date('d/m/Y H:i:s', mktime($heure, $minutes, $secondes, $mois, $jour, $annee)).'<br>';
+echo 'Le lendemain de l\'an 2000 : '.date('d/m/Y H:i:s', mktime($heure, $minutes, $secondes, $mois, date($jour + 1), $annee)).'<br>';
+echo 'Le mois suivant du jour de l\'an... : '.date('d/m/Y H:i:s', mktime($heure, $minutes, $secondes, date($mois + 1), $annee)).'<br>';
+echo mktime(8, 15, 0, null, -1);
+echo date('H:i', mktime(8, 5, null, null, null, null)).'<br>';
+echo date('d/m/Y', mktime(0, 0, 0, 4, 1, 2020)).'<br>';
