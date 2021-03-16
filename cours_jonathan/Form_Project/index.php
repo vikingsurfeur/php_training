696d8eaf667009ee56cae8cc39a7a02e36fdde17
David Bouscarle <d.bouscarle@gmail.com>
2021-03-16 14:01:08 +0100

    Cours Jonathan Form Project to be continued

--- cours_jonathan/Form_Project/index.php
+++ cours_jonathan/Form_Project/index.php
@@ -1,9 +1,8 @@
 <?php
-
 require_once 'Form.php';
 require_once 'Conversion.php';
 $form = new Form($_POST);
-
+include_once 'header.php';
 ?>
 
 <form action="#" method="post">
@@ -11,9 +10,12 @@ $form = new Form($_POST);
         echo $form->input('username','text');
         echo $form->input('password', 'password');
         echo $form->submit();
-        var_dump($_POST['username']);
-        var_dump($_POST['password']);
+//        var_dump($_POST['username']);
+//        var_dump($_POST['password']);
         echo '<br>';
         echo Conversion::getZero(9); // Call my static function with :: in Conversion Object without instance
     ?>
 </form>
+
+<?php
+include_once 'footer.php';
